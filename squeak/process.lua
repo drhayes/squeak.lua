0bf3726176c721760b46481ad996f263aa8d5daf
David Hayes <drhayes@omg.lol>
2023-06-18 23:58:24 -0500

    Very basic process structure.
    Totally stolen from deepnight's deepnightLibs.

---
+++ squeak/process.lua
@@ -0,0 +1,67 @@
+local Object = require('lib.classic')
+local lume = require('lib.lume')
+
+local Process = Object:extend()
+
+function Process:new(parent)
+  self.childProcesses = {}
+  self.parent = nil
+  self.enabled = true
+  if parent then
+    parent:attach(self)
+  elseif not self.parent then
+    table.insert(Process.roots, self)
+  end
+end
+
+function Process:attach(child)
+  if child.parent == nil then
+    lume.remove(Process.roots, child)
+  else
+    lume.remove(child.parent.childProcesses, child)
+  end
+  child.parent = self
+  table.insert(self.childProcesses, child)
+end
+
+function Process:canRun()
+  return self.enabled
+end
+
+function Process.runPreUpdate(p, dt)
+  if not p:canRun() then return end
+  p:preUpdate(dt)
+  local childProcesses = p.childProcesses
+  for i = 1, #childProcesses do
+    local childProcess = childProcesses[i]
+    Process.runPreUpdate(childProcess, dt)
+  end
+end
+
+function Process.runUpdate(p, dt)
+  if not p:canRun() then return end
+  p:update(dt)
+  local childProcesses = p.childProcesses
+  for i = 1, #childProcesses do
+    local childProcess = childProcesses[i]
+    Process.runUpdate(childProcess, dt)
+  end
+end
+
+function Process.runPostUpdate(p, dt)
+  if not p:canRun() then return end
+  p:postUpdate(dt)
+  local childProcesses = p.childProcesses
+  for i = 1, #childProcesses do
+    local childProcess = childProcesses[i]
+    Process.runPostUpdate(childProcess, dt)
+  end
+end
+
+function Process:preUpdate(dt) end
+function Process:update(dt) end
+function Process:postUpdate(dt) end
+
+Process.roots = {}
+
+return Process
